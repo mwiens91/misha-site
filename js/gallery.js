07700df2f894c2a7b196c77bb6a1558b07ddad30
Matt Wiens <mwiens91@gmail.com>
2021-10-07 01:54:30 -0400

    Prepend a / to pics paths

--- js/gallery.js
+++ js/gallery.js
@@ -2,35 +2,35 @@ jQuery(document).ready(function () {
   jQuery("#gallery").nanogallery2({
     items: [
       {
-        src: "20211006_111732.jpg",
+        src: "/20211006_111732.jpg",
         srct: "/thumbs/20211006_111732.jpg",
       },
       {
-        src: "20211006_111654.jpg",
+        src: "/20211006_111654.jpg",
         srct: "/thumbs/20211006_111654.jpg",
       },
       {
-        src: "20211006_110903.jpg",
+        src: "/20211006_110903.jpg",
         srct: "/thumbs/20211006_110903.jpg",
       },
       {
-        src: "20211005_182707.jpg",
+        src: "/20211005_182707.jpg",
         srct: "/thumbs/20211005_182707.jpg",
       },
       {
-        src: "20211005_173831.jpg",
+        src: "/20211005_173831.jpg",
         srct: "/thumbs/20211005_173831.jpg",
       },
       {
-        src: "20211005_163315.jpg",
+        src: "/20211005_163315.jpg",
         srct: "/thumbs/20211005_163315.jpg",
       },
       {
-        src: "20211005_143207.jpg",
+        src: "/20211005_143207.jpg",
         srct: "/thumbs/20211005_143207.jpg",
       },
       {
-        src: "20211005_143100.jpg",
+        src: "/20211005_143100.jpg",
         srct: "/thumbs/20211005_143100.jpg",
       },
     ],
